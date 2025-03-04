8ef453c4e9cc0393df10c5fb6a029d848f7172cb
Duy Thuan <boooyoung152@gmail.com>
2025-03-04 23:05:52 +0700

    Duy Thuan | Add File_B.js
    Calculate BMI

---
+++ File_B.js
@@ -0,0 +1,13 @@
+
+// Calulates BMI ( Boday Mass Index )
+
+// Variables for weight and height
+let weight = 95;
+let height = 1.74;
+
+// Calculate BMI
+let bmi = weight / ( height * height );
+
+// Print the result
+console.log("Your Body Mass Index (BMI) is: " + bmi.toFixed(2));
+
